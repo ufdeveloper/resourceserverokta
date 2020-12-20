3d14b0e5ab4b85eb0833df129d6b9c39219c9035
Shantanu Sardal <ufdeveloper@gmail.com>
2020-12-20 17:00:04 -0500

    added proper documentation

--- src/main/java/org/shanmeg/resourceserverokta/controller/ResourceServerController.java
+++ src/main/java/org/shanmeg/resourceserverokta/controller/ResourceServerController.java
@@ -14,6 +14,9 @@ import java.security.Principal;
 @RestController
 public class ResourceServerController {
 
+    /**
+     * This is the right @AuthenticationPrincipal to use when using JWT authorization.
+     */
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String helloWorld(@AuthenticationPrincipal Jwt jwt) {
         log.info("got request including jwt {}", jwt);
@@ -23,6 +26,13 @@ public class ResourceServerController {
         return "Hello " + (holderName != null ? holderName : "anonymous");
     }
 
+    /**
+     * The principal will not be resolved to an OidcUser object.
+     * See {@link org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver#resolveArgument} for how the annotation works.
+     * In short, when using Jwt authorization, the {@link org.springframework.security.core.Authentication} object in the {@link org.springframework.security.core.context.SecurityContextHolder}
+     * is a {@link org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken}. The principal in this case is a {@link Jwt} object.
+     * This object is not assignable to {@link OidcUser}, and the resolver returns null.
+     */
     @RequestMapping(path = "/helloOidcUser", method = RequestMethod.GET)
     public String helloWorldOidcUser(@AuthenticationPrincipal OidcUser oidcUser) {
         log.info("got request including oidcUser {}", oidcUser);
@@ -33,6 +43,9 @@ public class ResourceServerController {
         return "Hello " + (name != null ? name : "anonymous");
     }
 
+    /**
+     * The principal will not be resolved to an OidcUser object.
+     */
     @RequestMapping(path = "/helloOidcUserNoAnnotation", method = RequestMethod.GET)
     public String helloWorldOidcUserNoAnnotation(OidcUser oidcUser) {
         log.info("got request including oidcUser {}", oidcUser);
@@ -43,6 +56,10 @@ public class ResourceServerController {
         return "Hello " + (name != null ? name : "anonymous");
     }
 
+    /**
+     * Since {@link Principal} is the superclass, this works. The {@link org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken}
+     * class will be the actual principal subclass used here.
+     */
     @RequestMapping(path = "/helloPrincipal", method = RequestMethod.GET)
     public String helloWorld(Principal principal) {
         log.info("got request including principal {}", principal);
@@ -50,6 +67,9 @@ public class ResourceServerController {
         return "Hello " + principal.getName();
     }
 
+    /**
+     *  This does not work. Similar to {@link #helloWorldOidcUser(OidcUser)} above, the {@link Jwt} object is not assignable to {@link Principal}.
+     */
     @RequestMapping(path = "/helloAuthenticationPrincipal", method = RequestMethod.GET)
     public String helloAuthenticationPrincipal(@AuthenticationPrincipal Principal principal) {
         log.info("got request including AuthenticationPrincipal {}", principal);
